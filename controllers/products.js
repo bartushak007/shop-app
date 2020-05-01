abb8f8ec5a155bab1f7ca0015eff671c2883af33
Roman <rmnbrtshk@gmail.com>
2020-05-01 11:33:18 +0300

    weird

--- controllers/products.js
+++ controllers/products.js
@@ -69,11 +69,11 @@ class Products {
       const jwtData = jwt.verify(token, process.env.JWT_KEY);
       if (jwtData.id !== id) throw new Error("do not logined");
       const products = await ProductsModel.find({ user_id: id })
-        .skip(skip = 0)
-        .limit(limit = 5)
-        .populate({          
-          options: { sort: { date: -1 } },
-        });
+        // .skip(skip = 0)
+        // .limit(limit = 5)
+        // .populate({          
+        //   options: { sort: { date: -1 } },
+        // });
       res.status(200).json({
         success: true,
         data: products,
